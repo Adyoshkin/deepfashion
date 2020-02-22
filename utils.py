2cdec890e6d61e5eb566cfbe957bcd80513bb9ec
Adyoshkin Vasiliy <adeshkin.vi@phystech.edu>
2020-02-22 20:24:32 +0300

    add tools for visualization

---
+++ utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+from config import BASE_PATH
+
+def get_path_id():
+    path_to_idx = {}
+    idx_to_path = {}
+    bbox_file = open(BASE_PATH + 'anno/list_bbox.txt', 'r').readlines()
+    bbox = [[int(v) if i > 0 else v for i, v in enumerate(list(filter(None, x[:-1].split(' '))))] for x in bbox_file[2:]]
+    for i, cur_bbox in enumerate(bbox):
+        path = cur_bbox[0]
+        idx_to_path[i] = path
+        path_to_idx[path] = i
+    
+    return path_to_idx, idx_to_path
+
+def get_bbox():
+    bbox_file = open(BASE_PATH + 'anno/list_bbox.txt', 'r').readlines()
+    bbox = [[int(v) if i > 0 else v for i, v in enumerate(list(filter(None, x[:-1].split(' '))))] for x in bbox_file[2:]]
+    bbox = np.array([ln[1:] for ln in bbox], dtype=np.int32)
+    
+    return bbox
+    
+
+def get_categories():
+    cats_file = open(BASE_PATH + 'anno/list_category_cloth.txt', 'r').readlines()
+    categories = []
+    for ln in cats_file[2:]:
+        cur = list(filter(None, ln[:-1].split(' ')))
+        categories.append(cur[0])
+    
+    return categories
+
+
+def vis_utils(flg):
+    path_to_idx, idx_to_path = get_path_id()
+    bbox = get_bbox()
+    categories = get_categories()
+    
+    cats_img_file = open(BASE_PATH + 'anno/list_category_img.txt', 'r').readlines()
+    cat_target = np.zeros((len(idx_to_path), len(categories)), dtype=np.uint8)
+    cat_list = {}
+
+    for ln in cats_img_file[2:]:
+        cur = list(filter(None, ln[:-1].split(' ')))
+        cur_cat = int(cur[1]) - 1
+
+        cat_target[path_to_idx[cur[0]]][cur_cat] = 1
+        if cur_cat in cat_list:
+            cat_list[cur_cat].append(path_to_idx[cur[0]])
+        else:
+            cat_list[cur_cat] = [path_to_idx[cur[0]]]
+            
+    if flg == 'category':
+        return idx_to_path, categories, cat_list
+    else if flg == 'img':
+        return idx_to_path, bbox, categories, cat_target
---
+++ vis.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import random
+from skimage.io import imread
+import matplotlib.patches as patches
+
+from config import BASE_PATH
+from utils import vis_utils
+
+def vis_category(idx=None, name=None, cols=4, rows=4):
+    idx_to_path, categories, cat_list = vis_utils('category')
+    
+    if idx is None:
+        cur_lst = cat_list[categories.index(name)]
+    else:
+        cur_lst = cat_list[idx]
+    random.shuffle(cur_lst)
+    plt.figure(figsize=(8, 8))
+    
+    plt.suptitle('Category "%s"' % name, fontsize=22)
+    for cur_col in range(cols):
+        for cur_row in range(rows):
+            idx = cur_row * cols + cur_col 
+            plt.subplot(rows, cols, idx + 1)
+            plt.axis('off')
+            plt.imshow(imread(BASE_PATH + idx_to_path[cur_lst[idx]]))  
+    plt.subplots_adjust(wspace=0.0, hspace=0.2)
+
+def vis_img(path=None, idx=None):
+    idx_to_path, bbox, categories, cat_target = vis_utils('img')
+    
+    plt.figure(figsize=(6, 8))
+    if idx is not None:
+        cur_path = idx_to_path[idx]
+        cur_idx = idx
+        category = categories[cat_target[cur_idx].argmax()]
+    else:
+        cur_path = path
+        cur_idx = path_to_idx[path]
+        category = categories[cat_target[cur_idx].argmax()]
+    
+    plt.imshow(imread(BASE_PATH + cur_path))
+
+    p1, p2 = bbox[cur_idx][:2], bbox[cur_idx][2:]
+    rect = patches.Rectangle((bbox[cur_idx][[0, 2]].min(), bbox[cur_idx][[1, 3]].min()), 
+                             np.abs((p1-p2))[0], np.abs((p1-p2))[1], linewidth=1, edgecolor='r', facecolor='none')
+    plt.gca().add_patch(rect)
+    plt.title('Category: %s\n' % category)
