32d676a54c2f9035e0a758c0c95e52ff26f428e4
Adyoshkin Vasiliy <adeshkin.vi@phystech.edu>
2020-02-22 12:06:09 +0300

    fix predict.py

--- predict.py
+++ predict.py
@@ -1,21 +1,18 @@
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 from dataloader import DirectoryIteratorWithBoundingBoxes
-from get_bbox import get_dict_bboxes
 
 def custom_generator(iterator):
     while True:
         batch_x, batch_y = iterator.next()
         yield (batch_x, batch_y)
         
-def predict():
+def predict(dict_test):
     model = load_model("/content/drive/My Drive/deepfashion/models/model.h5")
-    
-    dict_train, dict_val, dict_test = get_dict_bboxes()
     test_datagen = ImageDataGenerator()
-    test_iterator = DirectoryIteratorWithBoundingBoxes("./img/test", test_datagen, bounding_boxes=dict_test, 
-                                                       target_size=(200, 200))
+    test_iterator = DirectoryIteratorWithBoundingBoxes("./img/test", test_datagen,
+                                                       bounding_boxes=dict_test, target_size=(200, 200))
     
-    preds = model.predict_generator(custom_generator(test_iterator))
+    preds = model.predict_generator(custom_generator(test_iterator), steps=10)
     
     return preds
