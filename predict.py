b95b24a3889af49b8727544b05bd98d49600602b
Adyoshkin Vasiliy <adeshkin.vi@phystech.edu>
2020-02-23 23:20:51 +0300

    fix vis.py

--- predict.py
+++ predict.py
@@ -1,18 +1,24 @@
-from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
+import os
+from keras.models import load_model
+from skimage.io import imsave
+from get_bbox import get_dict_bboxes
 from dataloader import DirectoryIteratorWithBoundingBoxes
+from config import TEST_DIR, TARGET_SIZE, MODEL_PATH, SAVE_DIR, CATEGORIES
 
-def custom_generator(iterator):
-    while True:
-        batch_x, batch_y = iterator.next()
-        yield (batch_x, batch_y)
-        
-def predict(dict_test):
-    model = load_model("/content/drive/My Drive/deepfashion/models/model.h5")
+def vis_pred():
+    model = load_model(MODEL_PATH)
+
+    imgs, (labels, bboxs) =  test_iterator.next()
+    pred_labels, pred_bboxs = model.predict_on_batch(imgs)
+    bboxs = bboxs * 200
+    pred_bboxs = pred_bboxs * 200
+    dict_test = get_dict_bboxes(mode='test')
     test_datagen = ImageDataGenerator()
-    test_iterator = DirectoryIteratorWithBoundingBoxes("./img/test", test_datagen,
-                                                       bounding_boxes=dict_test, target_size=(200, 200))
+    test_iterator = DirectoryIteratorWithBoundingBoxes(TEST_DIR, test_datagen, 
+                                                       bounding_boxes=dict_test, target_size=TARGET_SIZE)
     
-    preds = model.predict_generator(custom_generator(test_iterator), steps=10)
-    
-    return preds
+    os.mkdir(SAVE_DIR)
+    for i, img in enumerate(imgs):
+        imsave(f'{SAVE_DIR}/{i}.png', img.astype(np.uint8))
+        
--- vis.py
+++ vis.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 import random
+from keras.models import load_model
 import numpy as np
-from skimage.io import imread
+from skimage.io import imread, imsave
 import matplotlib.patches as patches
+from keras.preprocessing.image import ImageDataGenerator   
 
-from config import BASE_DIR
+from get_bbox import get_dict_bboxes
+from dataloader import DirectoryIteratorWithBoundingBoxes
+from config import BASE_DIR, SAVE_DIR, CATEGORIES, TEST_DIR, TARGET_SIZE, MODEL_PATH
 from utils import vis_utils
 
     
@@ -47,3 +51,45 @@ def vis_img(path=None, idx=None):
                              np.abs((p1-p2))[0], np.abs((p1-p2))[1], linewidth=1, edgecolor='r', facecolor='none')
     plt.gca().add_patch(rect)
     plt.title('Category: %s\n' % category)
+
+def vis_pred(cols=4, rows=4):
+    model = load_model(MODEL_PATH)
+    
+    dict_test = get_dict_bboxes(mode='test')
+    test_datagen = ImageDataGenerator()
+    test_iterator = DirectoryIteratorWithBoundingBoxes(TEST_DIR, test_datagen, 
+                                                       bounding_boxes=dict_test, target_size=TARGET_SIZE)
+
+    imgs, (labels, bboxs) =  test_iterator.next()
+    pred_labels, pred_bboxs = model.predict_on_batch(imgs)
+    bboxs = bboxs * 200
+    pred_bboxs = pred_bboxs * 200
+    
+    os.mkdir(SAVE_DIR)
+    for i, img in enumerate(imgs):
+        imsave(f'{SAVE_DIR}/{i}.png', img.astype(np.uint8))
+        
+    plt.figure(figsize=(16, 16))    
+    for cur_col in range(cols):
+        for cur_row in range(rows):
+            idx = cur_row * cols + cur_col
+            ind = idx
+            bbox = bboxs[ind]
+            pred_bbox = pred_bboxs[ind]
+            label = CATEGORIES[labels[ind].argmax()]
+            pred_label = CATEGORIES[pred_labels[ind].argmax()]
+            plt.subplot(rows, cols, idx + 1)
+            plt.axis('off')
+            plt.title(f"true: {label}, pred: {pred_label}")
+            plt.imshow(imread(f'{SAVE_DIR}/{ind}.png'))
+            p1, p2 = bbox[:2], bbox[2:]
+            rect = patches.Rectangle((bbox[[0, 2]].min(), bbox[[1, 3]].min()), 
+                                      np.abs((p1-p2))[0], np.abs((p1-p2))[1], linewidth=1, edgecolor='r', facecolor='none')
+
+            pred_p1, pred_p2 = pred_bbox[:2], pred_bbox[2:]
+            pred_rect = patches.Rectangle((pred_bbox[[0, 2]].min(), pred_bbox[[1, 3]].min()), 
+                                          np.abs((pred_p1-pred_p2))[0], np.abs((pred_p1-pred_p2))[1], 
+                                          linewidth=1,edgecolor='b', facecolor='none')
+            plt.gca().add_patch(rect)
+            plt.gca().add_patch(pred_rect)
+            plt.subplots_adjust(wspace=0.0, hspace=0.2)
